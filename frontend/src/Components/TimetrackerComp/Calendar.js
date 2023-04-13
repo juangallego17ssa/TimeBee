8e2d4e6fef9d61c6b9cbe95024afef7b47377879
July04 <fnuffel@hotmail.com>
2023-04-13 17:47:48 +0200

    Calendar steps

---
+++ frontend/src/Components/TimetrackerComp/Calendar.js
@@ -0,0 +1,21 @@
+import React, { useRef, useState } from 'react';
+import { useEffect } from 'react';
+
+
+
+
+function Calendar() {
+
+    let [timer, setTime] = useState(0);
+    const intervalId = useRef()
+   
+   
+
+    return (
+        <div>
+            <div>Calendar</div>
+            <a href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=8b24d741-b41a-4d56-a4d5-364930a6ade0&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ftimetracker&response_mode=query&scope=offline_access%20user.read%20Calendars.ReadWrite&state=46216">Connect to Outloos</a>
+        </div>
+    )
+}
+export default Calendar;
