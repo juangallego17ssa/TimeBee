80061b16da34159a0fbb928116ce9f410a5f66fc
Peevee66 <peter.vajk@gmail.com>
2023-04-15 19:47:16 +0200

    public holiday fixed with new model structure and new serializer and view

--- backend/public_holiday/admin.py
+++ backend/public_holiday/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from public_holiday.models import PublicHoliday
+from public_holiday.models import Holiday, PublicHoliday
+from public_holiday.views import PublicHolidayViewSet
 
-admin.site.register(PublicHoliday)
+
+class PublicHolidayAdmin(admin.ModelAdmin):
+    list_display = ('holiday', 'year', 'date')
+    list_filter = ('year',)
+    actions = ['update_public_holidays']
+
+    def update_public_holidays(self, request, queryset):
+        view = PublicHolidayViewSet()
+        response = view.update_public_holidays(request)
+        self.message_user(request, response.data['message'])
+
+    update_public_holidays.short_description = "Update public holidays"
+
+
+admin.site.register(Holiday)
+admin.site.register(PublicHoliday, PublicHolidayAdmin)
