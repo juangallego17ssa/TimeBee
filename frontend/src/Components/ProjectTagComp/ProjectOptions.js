a933843757897bce68dc04e798798f663858e84a
Yuru Huang <ruby_huang@gmx.ch>
2023-04-18 10:06:13 +0200

    fetch project

--- frontend/src/Components/ProjectTagComp/ProjectOptions.js
+++ frontend/src/Components/ProjectTagComp/ProjectOptions.js
@@ -3,10 +3,22 @@ import AddNewProject from './AddNewProject';
 import {AiFillTag} from 'react-icons/ai'
 import { RxCross2 } from 'react-icons/rx' 
 
+//RTK
+import { useGetOwnProjectsQuery } from '../../api/API';
+
+
 function ProjectOptions({setTag}) {
-  const [projects,setProjects] = useState([{id:1,name:'project1',color:'purple'}])
+
+  const { data : projects=[], isLoading, error, refetch } = useGetOwnProjectsQuery()
+
+  
   const [ showCreateTag, setShowCreateTag ]= useState(false)
 
+  const handleSetProject=(project)=>{
+    console.log(project)
+  }
+
+  if (isLoading){return(<div>Loading</div>)}
 
   return (
     <div className=' z-10 '>
@@ -15,7 +27,8 @@ function ProjectOptions({setTag}) {
           <input className='m-4 border-2 rounded-full py-1 px-4 focus:outline-teal-400' id='project-filter' placeholder='Find project...'/>
         </label>
         {projects.map(project=>
-        <div className='grid grid-cols-[30px_1fr_10px] items-center hover:bg-stone-100 mb-2 px-4'>
+        <div className='grid grid-cols-[30px_1fr_10px] items-center hover:bg-stone-100 mb-2 px-4'
+              onClick={()=>handleSetProject(project)}>
           <AiFillTag className={`m-1 text-${project.color}-500`} />
           <p className='' key={project.id} onClick={setTag}>{project.name}</p> 
           <RxCross2 className='text-zinc-200 hover:text-zinc-900'/>
@@ -28,7 +41,7 @@ function ProjectOptions({setTag}) {
       </div>
       {showCreateTag && 
         <div className='fixed top-1/3 left-1/3'>
-          <AddNewProject setShowCreateTag={setShowCreateTag} projects={projects} setProjects={setProjects}/>
+          <AddNewProject setShowCreateTag={setShowCreateTag} projects={projects}/>
         </div>
           }
     </div>
