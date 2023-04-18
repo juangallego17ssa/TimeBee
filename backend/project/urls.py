13d2016268ecb72a844433e94b7de1f8b8354296
juangallego17 <juangallego17@hotmail.com>
2023-04-18 14:32:25 +0200

    implemented the creation of a default project and corrected the validation

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from project.views import ListCreateProjectView, RetrieveUpdateDeleteProjectView, ListOwnProjectView
+from project.views import ListCreateProjectView, RetrieveUpdateDeleteProjectView, ListOwnProjectView, \
+    CreateByUsernameView
 
 urlpatterns = [
     # backend/api/restaurants/
     path('', ListCreateProjectView.as_view()),
     path('own/', ListOwnProjectView.as_view()),
     path('<int:project_id>/', RetrieveUpdateDeleteProjectView.as_view()),
-
+    path('createbyusername/', CreateByUsernameView.as_view()),
 ]
