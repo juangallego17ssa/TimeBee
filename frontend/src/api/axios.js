fe437ba2f519bba940f485c81aaecd760ddc011a
juangallego17 <juangallego17@hotmail.com>
2023-04-19 12:33:18 +0200

    migration made

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -18,7 +18,7 @@ export const axiosWithToken = axios.create({
   baseURL: baseURL,
   timeout: 10000,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+    Authorization: `Bearer ${localStorage.getItem("access")}`,
     "Content-Type": "application/json",
   },
 });
