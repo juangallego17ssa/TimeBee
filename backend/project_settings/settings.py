122dc873f77d63f230a5fadc7e4066201cfe1809
Peevee66 <peter.vajk@gmail.com>
2023-04-19 14:46:25 +0200

    timezone fixed

--- backend/project_settings/settings.py
+++ backend/project_settings/settings.py
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "Europe/Zurich"
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Zurich"
 
 USE_I18N = True
 
