13cb9d2cbdc43e0087bb4a30aabee2044acc9c14
Yuru Huang <ruby_huang@gmx.ch>
2023-04-21 16:44:37 +0200

    get user name from store

--- frontend/src/Components/UserAvator.js
+++ frontend/src/Components/UserAvator.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {useGetUserProfileQuery} from '../api/API'
+import { useSelector } from 'react-redux';
 
 export default function UserAvator() {
-    const {data:currentUser,isLoading,isSuccess,isError} = useGetUserProfileQuery()
+   const USER = useSelector( (state) => state.user.user)
+  //  console.log(USER.username.charAt(0).toUpperCase())
+
+    // const {data:currentUser,isLoading,isSuccess,isError} = useGetUserProfileQuery()
     // console.log(currentUser)
+  if(USER)
   return (
     <div className="relative w-10 h-10 bg-teal-400 rounded-full flex justify-center items-center hover:opacity-70 hover:cuersor">
-      {currentUser?.avatar?
-      <img src={currentUser.avatar}/>
+      {USER?.avatar?
+      <img src={USER.avatar}/>
       :
       <p className='uppercase text-center text-2xl font-bold text-white'>
-        {currentUser?.username.charAt(0)}
+        {USER.username.charAt(0).toUpperCase()}
       </p>
       }
     </div>
