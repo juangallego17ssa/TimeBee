0ca30c870d04999ab0e3e68470022739604164de
July04 <fnuffel@hotmail.com>
2023-04-22 09:05:37 +0200

    last commit for graphs

--- frontend/src/Components/DashboadComp/DataProject.js
+++ frontend/src/Components/DashboadComp/DataProject.js
@@ -6,6 +6,9 @@ import { fetchTrackedTimeOwn } from '../../redux/Slices/trackedTimeOwnSlice';
 import moment from "moment";
 import RadialChart from './RadialChart';
 import MyResponsiveBar from './BarChart';
+import { axiosWithToken } from "../../api/axios";
+import Calendar from 'react-calendar';
+import "../ReportComp/Calendar_styles.css";
 
 
 function DataProject() {
@@ -19,17 +22,59 @@ function DataProject() {
     
     }, []);
     
-    const [selectedDate, setSelectedDate] = useState('')
+    const [selectedDate, setSelectedDate] = useState('');
+    const [selectedOption, setSelectedOption] = useState('week');
+    const [trackedTimerange, setTrackedTimeRange] = useState([]);
+    const [value, onChange] = useState(new Date());
+
+    function handleOptionChange(event) {
+        setSelectedOption(event.target.value);
+    }
     
     // console.log(reduxTrackedTime)
+    //------------------Select Date by Datepicker-----------------------
 
-//--------Filtering Data by Week, Month, Year--------
+    console.log(value)
+    let todayPick = value[1];
+    let startdayPick = value[0];
+
+    console.log(todayPick)
+    console.log(startdayPick)
+
+    //--------Filtering Data by Week, Month, Year with select--------
+    const today = new Date();
+
+    let startday;  
+
+    if (selectedOption === 'week') {
+        const OneWeekAgo = new Date();
+        OneWeekAgo.setDate(today.getDate()-7)
+        startday = OneWeekAgo;
+    } else if (selectedOption === 'month') {
+        const oneMonthAgo = new Date();
+        oneMonthAgo.setMonth(today.getMonth() - 1);
+        startday = oneMonthAgo;
+    } else if (selectedOption === 'year') {
+        const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+        startday = oneYearAgo;
+    }
     
+    // console.log(startday)
+    ///trackedtime/?start_date=2023-04-01&end_date=2023-04-30 endpoint to get user info an all tracked times
 
-
-
-
-
+    useEffect(() => {
+        const endDateString = moment(today).format('YYYY-MM-DD');
+        const startDateString = moment(startday).format('YYYY-MM-DD');
+        const getTrackedTime = async () => {
+               const response = await axiosWithToken(`trackedtime/?start_date=${startDateString}&end_date=${endDateString}`)
+               setTrackedTimeRange(response.data)   
+           }
+          getTrackedTime()
+        
+        //   console.log(startDateString)
+        //   console.log(endDateString)
+        
+      }, [selectedOption])
 
  //--------FOR BAR CHART-------------------------------------------------    
 // Filter all Task by project and by Day and not Clock in/out
@@ -53,7 +98,7 @@ function DataProject() {
         dataTimeOwn[day] = dataTimeOwn[day].sort((a, b) => b.duration.asMilliseconds() - a.duration.asMilliseconds());
         const projectDurations = dataTimeOwn[day].reduce((acc, item) => {
         const { project, duration } = item;
-        const hours = parseFloat((duration.asMinutes() / 60).toFixed(2)); // hours in decimal round to 2 digits
+        const hours = Math.abs(parseFloat((duration.asMinutes() / 60).toFixed(2))); // hours in decimal round to 2 digits
         // const minutes = Math.floor(duration.asMinutes() % 60);
         // const formattedDuration = `${hours}h ${minutes}m`;
         if (acc[project]) {
@@ -116,7 +161,7 @@ function DataProject() {
     for (const day in dataTimeOwnSum) {
         dataTimeOwnSum[day].forEach(item => {
             const { project, duration } = item;
-            const hours = parseFloat((duration.asMinutes() / 60).toFixed(2));
+            const hours = Math.abs(parseFloat((duration.asMinutes() / 60).toFixed(2)));
             if (projectDurations[project]) {
                 projectDurations[project] += hours;
             } else {
@@ -146,7 +191,7 @@ function DataProject() {
              const project = item.project.name;
              const task_name = item.task_name;
              const duration = moment.duration(moment(item.stop).diff(moment(item.start)));
-             const hours = parseFloat((duration.asMinutes() / 60).toFixed(2));
+             const hours = Math.abs(parseFloat((duration.asMinutes() / 60).toFixed(2)));
     
              // Add the item to the array for the current day
              if (!acc[project]) {
@@ -185,7 +230,19 @@ function DataProject() {
     // Create Barchart
       <div className=" Container flex flex-col flex-grow bg-stone-100 w-full md:h-screen px-8 py-4">
         <div className='flex flex-col '>
-            <input type="date" />
+        <Calendar onChange={onChange} value={value} showWeekNumbers={true} 
+          selectRange={true} 
+          defaultValue={value} />
+          </div>
+        <div>
+            <select value={selectedOption} onChange={handleOptionChange}>
+                <option value="week">Week</option>
+                <option value="month">Month</option>
+                <option value="year">Year</option>
+            </select>
+                {/* <p>You selected {selectedOption}</p> */}
+                <p>Start date: {moment(startday).format('YYYY-MM-DD')}</p>
+                <p>End date: {moment(today).format('YYYY-MM-DD')}</p>
         </div>
         <BarChart data={newdata} keys={projects}/>
         <PieChart data={dataPie} />
