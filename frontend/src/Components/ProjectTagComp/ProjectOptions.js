7985fb6d435b62322a1ec0e84f1c517d705c21ce
Yuru Huang <ruby_huang@gmx.ch>
2023-04-23 11:53:37 +0200

    add search project function

--- frontend/src/Components/ProjectTagComp/ProjectOptions.js
+++ frontend/src/Components/ProjectTagComp/ProjectOptions.js
@@ -19,10 +19,16 @@ function ProjectOptions({setTag, setSelectedProject,setShowProjectTags}) {
     // console.log(projects)
   
   const [deleteProjectByID] = useDeleteProjectByIDMutation();
+  const [isSearch,setIsSearch] = useState(false);
+  const [filteredProjects,setFilteredProject] = useState([]);
 
-
-  
   const [ showCreateTag, setShowCreateTag ]= useState(false)
+
+  const handleFilterProject = (e)=>{
+    const all_projects = projects
+    const filtered = all_projects.filter(project=>project.name.includes(e.target.value))
+    setFilteredProject(filtered)
+  }
 
   const handleSetProject=(project)=>{
     setSelectedProject(project)
@@ -39,23 +45,49 @@ function ProjectOptions({setTag, setSelectedProject,setShowProjectTags}) {
     <div className=' z-20 '>
       <div className='absolute right-0 top-10 width-40 bg-white rounded-md shadow-md'>
         <label htmlFor='project-filter'>
-          <input className='m-4 border-2 rounded-full py-1 px-4 focus:outline-teal-400' id='project-filter' placeholder='Find project...'/>
+          <input className='m-4 border-2 rounded-full py-1 px-4 focus:outline-teal-400' 
+          id='project-filter'
+          onChange={handleFilterProject}
+          onClick={()=>setIsSearch(!isSearch)}
+          placeholder='Find project...'
+          />
+          
         </label>
-        {projects.map(project=>
-        <div key={project.id}
-        className='grid grid-cols-[30px_1fr_10px] items-center hover:bg-stone-100 mb-2 px-4'
-              >
-          <AiFillTag className={`m-1 text-${project.tag_color?project.tag_color:'zinc'}-400`} />
-          <p className='' key={project.id} 
-            onClick={()=>handleSetProject(project)}>
-          {project.name}</p> 
-            
-          <RxCross2 
-            onClick={()=>handleDeteleProject(project)}
-            className='text-zinc-200 hover:text-zinc-900'/>
+        { isSearch?
+          <div>
+            {filteredProjects.map(project=>
+            <div key={project.id}
+               className='grid grid-cols-[30px_1fr_10px] items-center hover:bg-stone-100 mb-2 px-4'
+                >
+            <AiFillTag className={`m-1 text-${project.tag_color?project.tag_color:'zinc'}-400`} />
+            <p className='' 
+              onClick={()=>handleSetProject(project)}>
+            {project.name}</p> 
+              
+            <RxCross2 
+              onClick={()=>handleDeteleProject(project)}
+              className='text-zinc-200 hover:text-zinc-900'/>
+            </div>
+            )}
 
-        </div>
-        )}
+          </div>
+          :
+          projects.map(project=>
+          <div key={project.id}
+          className='grid grid-cols-[30px_1fr_10px] items-center hover:bg-stone-100 mb-2 px-4'
+                >
+            <AiFillTag className={`m-1 text-${project.tag_color?project.tag_color:'zinc'}-400`} />
+            <p className='' key={project.id} 
+              onClick={()=>handleSetProject(project)}>
+            {project.name}</p> 
+              
+            <RxCross2 
+              onClick={()=>handleDeteleProject(project)}
+              className='text-zinc-200 hover:text-zinc-900'/>
+
+          </div>
+          )
+          }
         <div>
           <p className='m-4 text-teal-500 hover:cursor-pointer hover:font-semibold text-center capitalize'
           onClick={()=>setShowCreateTag(!showCreateTag)}>Create new project</p>
