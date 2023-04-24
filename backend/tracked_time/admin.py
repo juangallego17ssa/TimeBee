a4521d4020ee6af870b72375baf42a00c90703cf
Peevee66 <peter.vajk@gmail.com>
2023-04-24 18:25:22 +0200

    linting fixed

--- backend/tracked_time/admin.py
+++ backend/tracked_time/admin.py
@@ -3,7 +3,7 @@ from tracked_time.models import TrackedTime
 
 
 class TrackedTimeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'start', 'stop', 'task_name', 'code','get_username']
+    list_display = ['id', 'start', 'stop', 'task_name', 'code', 'get_username']
 
     def get_username(self, object):
         return object.project.created_by.username
